8517965c70fe86d2662d3fc712dd9ab9438464bf
RaffaeleSgarro <raffaeleSgarro@gmail.com>
2013-01-27 02:33:01 +0100

    Show standard deviation, but it's a mess

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,12 @@
+from __future__ import division
+
 import yaml
 import raf
 import alex
 import sys
 import time
 import pprint
+import math
 
 def main(filename):
   with open(filename, "r") as fd:
@@ -27,7 +30,7 @@ def stat (impls, data):
       assignments = f(data["tasks"], data["workers"])
       series.append(assignments)
     duration = time_millis() - start
-    print "%s took %dms, %.2fms on average" % (label, duration, float(duration) / times)
+    print "%s took %dms, %.2fms on average" % (label, duration, duration / times)
     examine_result(series, data)
 
 # Looks baaaaaaaaaaaaad there must be a library for this    
@@ -49,10 +52,23 @@ def examine_result(result, data):
       for task in tasks:
         work2[task][assignee] = work2[task][assignee] + 1
   pprint.pprint(work)
+  std_deviation = std_dev(work.values())
+  print "Standard deviation %.2f tasks, %.2f%%" % (std_deviation, std_deviation / avg(work.values()) * 100)
   pprint.pprint(work2)
     
 def time_millis():
   return int(round(time.time() * 1000))
+
+def avg(nums):
+  return sum(nums) / len(nums)
+
+def sqr(n):
+  return n * n
+
+def std_dev(nums):
+  mean = avg(nums)
+  return math.sqrt(sum(sqr(mean - n) for n in nums) / len(nums))
+  
 
 if __name__ == "__main__":
   main (sys.argv[1] if len(sys.argv) == 2 else "input.yml")
